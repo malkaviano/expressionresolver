e5fa94e57d3150025c7e6bb84dc2aa9d2543dc9f
Rafael Soares <rafael.kss@gmail.com>
2019-01-10 20:09:50 -0400

    tests green

--- src/test/scala/malkaviano/operators/comparison/LessThanOperatorSpec.scala
+++ src/test/scala/malkaviano/operators/comparison/LessThanOperatorSpec.scala
@@ -1,19 +1,19 @@
 package malkaviano.operators.comparison
 
-import malkaviano.operators.property.{OptionalProperty, ValueProperty}
+import malkaviano.operators.property.OptionalProperty
 import malkaviano.operators.value.Literal
 import org.joda.time.DateTime
 import org.scalatest.{FunSpec, Matchers}
 
 class LessThanOperatorSpec extends FunSpec with Matchers {
-  case class Fake(age: Int, birthdate: Option[DateTime] = None)
+  case class Fake(age: Int, birthdate: DateTime)
 
-  val fake = Fake(5)
+  val fake = Fake(5, DateTime.now)
 
   describe("Evaluation") {
     it("is true when first operand is less than second operand") {
       val operand1 = Literal(4)
-      val operand2 = ValueProperty[Int]("age", fake)
+      val operand2 = OptionalProperty[Int]("age", fake)
 
       val expr = LessThanOperator(operand1, operand2)
 
@@ -32,7 +32,7 @@ class LessThanOperatorSpec extends FunSpec with Matchers {
     describe("when using custom type with custom Ordering") {
       it("returns true") {
         val operand1 = OptionalProperty[DateTime]("birthdate", fake)
-        val operand2 = Literal(Option(DateTime.now.plusDays(1)))
+        val operand2 = Literal(DateTime.now.plusDays(1))
 
         import com.github.nscala_time.time.OrderingImplicits._
 
